16f49605b005771290f738480f5b027a0336a394
Kasim Te <kasimte@gmail.com>
2019-12-26 02:06:15 -0500

    New work.

---
+++ python/lists.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    N = int(input())
+    array = [] # could also be list()
+
+    while N > 0:
+        temp = input().split()
+        command = temp[0]
+        args = temp[1:]
+        if command == "print":
+            print(array)
+        else:
+            eval("array.{0}{1}".format(command,tuple(map(int,args))))
+        N = N - 1
