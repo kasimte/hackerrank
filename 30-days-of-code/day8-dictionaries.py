28680a92ff31489825a4901ea474928a9a39d055
Kasim Te <kasimte@gmail.com>
2019-12-29 14:57:50 -0500

    Fix code.

--- 30-days-of-code/day8-dictionaries.py
+++ 30-days-of-code/day8-dictionaries.py
@@ -1,3 +1,5 @@
+# https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
+
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
 n = int(input())
@@ -7,11 +9,14 @@ for x in range(0,n):
     line = input().split()
     book[line[0]] = line[1]
 
-for x in range(0,n):
-    line = input().split()
-    name = line[0]
-    if name in book:
-        entry = book[name]
-        print("{0}={1}".format(name,entry))
-    else:
-        print("Not found")
+while True:
+    try:
+        line = input().split()
+        name = line[0]
+        if name in book:
+            entry = book[name]
+            print("{0}={1}".format(name, entry))
+        else:
+            print("Not found")
+    except EOFError:
+        break
